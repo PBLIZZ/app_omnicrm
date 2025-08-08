bcac947d8bac064d57d7efbda0a69fd33c0c44c6
Peter James Blizzard <151367881+PBLIZZ@users.noreply.github.com>
2025-08-08 11:48:34 +0200

    chore(lint): enable TS strict, ESLint, Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("plugin:@typescript-eslint/recommended", "prettier"),
+  {
+    plugins: {
+      "@typescript-eslint": await import("@typescript-eslint/eslint-plugin"),
+      "unused-imports": await import("eslint-plugin-unused-imports"),
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": ["warn", { ignoreRestArgs: true }],
+      "unused-imports/no-unused-imports": "error",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
 ];
 
 export default eslintConfig;
